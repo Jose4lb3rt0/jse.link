408e0fd0022496f5de44779808f12ee5520585f0
Jose4lb3rt0 <josealberto3200@gmail.com>
2025-10-20 09:04:49 -0500

    backend montado

---
+++ backend/src/config/db.ts
@@ -0,0 +1,10 @@
+const dbName = process.env.DATABASE_NAME;
+const dbUser = process.env.DATABASE_USER;
+const dbPassword = process.env.DATABASE_PASSWORD;
+const dbHost = process.env.DATABASE_HOST;
+const dbTable = process.env.DATABASE_TABLE;
+
+export const conectarDB = async () => {
+  try {
+  } catch (error) {}
+};
