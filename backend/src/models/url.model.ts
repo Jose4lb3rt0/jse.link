83e459b7a33ec32a6cf437aaaccfc6e753fe20b7
Jose4lb3rt0 <josealberto3200@gmail.com>
2025-10-25 12:59:00 -0500

    Agregar modelo de URL con esquema y validación, e incluir tipos para nanoid

---
+++ backend/src/models/url.model.ts
@@ -0,0 +1,50 @@
+import { Document, model, Schema } from "mongoose";
+import { nanoid } from "nanoid";
+
+export interface IURL extends Document {
+    shortId: string
+    originaUrl: string
+    clicks: number
+    createdAt: Date
+}
+
+const urlSchema = new Schema<IURL>(
+    {
+        shortId: {
+            type: String,
+            required: true,
+            unique: true,
+            default: () => nanoid(6)
+        },
+        originaUrl: {
+            type: String,
+            required: true,
+            trim: true,
+            validate: {
+                validator: (value: string) => {
+                    try {
+                        new URL(value)
+                        return true
+                    } catch (error) {
+                        return false
+                    }
+                },
+
+                message: "La URL proporcionada no es válida."
+            }
+        },
+        clicks: {
+            type: Number,
+            required: true,
+            default: 0,
+        },
+    },
+    {
+        timestamps: {
+            createdAt: true,
+            updatedAt: true
+        }
+    }
+)
+
+export default model<IURL>("URL", urlSchema)
