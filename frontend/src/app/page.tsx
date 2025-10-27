5f4fa37e2dd2f8d9e4e79a0ea1be839f698ca61b
Jose4lb3rt0 <josealberto3200@gmail.com>
2025-10-27 15:53:13 -0500

    Hook + Sonner Toast

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,23 +1,22 @@
 "use client"
 
-import { createUrl } from "@/services/UrlService"
-import { useMutation } from "@tanstack/react-query"
+import { useUrls } from "@/hooks/useUrls"
 import { FormEvent, useState } from "react"
+import { Toaster } from "sonner"
 
 export default function Home() {
   const [url, setUrl] = useState("")
+  const { createUrl } = useUrls()
 
-  const { mutate, data, error, isPending } = useMutation({
-    mutationFn: (originalUrl: string) => createUrl(originalUrl)
-  })
+  const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5000"
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault()
-    mutate(url)
+    createUrl.mutate(url)
   }
 
-  const shortUrl = data?.data?.shortId
-    ? `${window.location.origin}/${data.data.shortId}`
+  const shortUrl = createUrl.data?.data?.shortId
+    ? `${API_BASE_URL}/api/urls/${createUrl.data.data.shortId}`
     : null
 
   return (
@@ -33,25 +32,32 @@ export default function Home() {
             className="border px-2 py-1 rounded text-black w-full"
           />
           <button
-            disabled={isPending}
+            disabled={createUrl.isPending}
             type="submit"
             className="bg-blue-600 text-white p-2 rounded hover:bg-blue-700 shrink-0"
           >
-            {isPending ? "Acortando..." : "Acortar URL"}
+            {createUrl.isPending ? "Acortando..." : "Acortar URL"}
           </button>
         </form>
 
-        {error && <p className="text-red-500 mt-4">Error: {(error as Error).message}</p>}
+        {createUrl.error && <p className="text-red-500 mt-4">Error: {(createUrl.error as Error).message}</p>}
 
         {shortUrl && (
           <div className="mt-4">
             <p className="font-medium text-black">URL corta:</p>
-            <a href={shortUrl} className="text-blue-600 underline">
+            <a
+              href={shortUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-600 underline"
+            >
               {shortUrl}
             </a>
+
           </div>
         )}
       </div>
+      <Toaster />
     </div>
   )
 }
