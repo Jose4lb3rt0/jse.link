bd4ab46fc46c3576167c4ce4fdf6504a10ce1e4d
Jose4lb3rt0 <josealberto3200@gmail.com>
2025-10-28 13:01:52 -0500

    Redis implementado: Cacheado en puerto del WSL

--- backend/src/services/urlService.ts
+++ backend/src/services/urlService.ts
@@ -1,6 +1,7 @@
 import URL, { IURL } from "../models/url.model"
 import { Request, Response } from "express"
 import { ApiResponse } from "../types/Response"
+import redisClient from "../config/redisClient"
 
 export const createUrl = async (req: Request, res: Response<ApiResponse<IURL>>) => {
     try {
@@ -14,6 +15,9 @@ export const createUrl = async (req: Request, res: Response<ApiResponse<IURL>>) 
         }
 
         const nuevaUrl = await URL.create({ originalUrl })
+
+        await redisClient.set(nuevaUrl.shortId, nuevaUrl.originalUrl) // <-- key: value
+
         return res.status(201).json({
             success: true,
             message: "Se ha creado la URL correctamente.",
@@ -32,11 +36,22 @@ export const redirectToOriginal = async (req: Request, res: Response<ApiResponse
         const { shortId } = req.params // Es parámetro, no body
         if (!shortId) return res.status(404).json({ success: false, message: "No se utilizó ningún URL." })
 
+        // REDIS CACHÉ
+        const cachedURL = await redisClient.get(shortId) // agarrar la key
+        if (cachedURL) {
+            console.log(`⚡ URL obtenida desde Redis: ${cachedURL}`)
+            return res.redirect(cachedURL)
+        }
+
+        // BUSQUEDA EN MONGO
         const found = await URL.findOne({ shortId })
         if (!found) return res.status(404).json({ success: false, message: "URL no encontrada." })
 
         found.clicks++
         await found.save()
+
+        // SAVE EN REDIS
+        await redisClient.set(shortId, found.originalUrl)
 
         return res.redirect(found.originalUrl)
     } catch (error) {
